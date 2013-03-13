9b68231d51e81c3396b301e7c36f05092ab5b6d9
Chase Merlin <chasemerlin@gmail.com>
2013-03-13 14:56:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChaseTwitter::Application.config.secret_token = '284840c65923b8aa78b32d0a5012961166525863397bb6a7034fb1ec1f7e8dbb6ccc3087fd5519fa7c83ef3ec3034e9d5ba198982413cfcc4026d004d3a2b231'
