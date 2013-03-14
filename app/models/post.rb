220671f4b8b593cca949e52427f092447a9d70cb
Chase Merlin <chasemerlin@gmail.com>
2013-03-14 17:30:55 -0400

    add validations for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :private, :title
+
+  validates :title, :presence => true, :uniqueness => true
+  validates :content, :presence => true
 end
